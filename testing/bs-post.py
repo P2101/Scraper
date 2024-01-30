ea18014174df0d5834e12da53244ee7bda9ddf55
Pau Pons <98814765+P2101@users.noreply.github.com>
2024-01-30 18:23:05 +0100

    Update bs-post.py

--- testing/bs-post.py
+++ testing/bs-post.py
@@ -1,31 +1,5 @@
 from bs4 import BeautifulSoup
 import requests
-
-# def get_image(url, headers):
-#     response = requests.get(url, headers= headers).text
-#     soup = BeautifulSoup(response, 'html.parser')
-
-#     images = soup.find_all('img')
-#     for img in images:
-#         image = img.get('src')
-#         if 'Tiffany' in image:
-#             print(image.strip())
-
-
-
-# headers ={
-#     'authority': 'www.movistar.com.co',
-#     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-#     'accept-language': 'es-ES,es;q=0.9,en;q=0.8,ca;q=0.7',
-#     'cache-control': 'max-age=0',
-#     'upgrade-insecure-requests': '1',
-#     'user-agent':'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36',
-#     }
-# url = 'https://www.tiffany.es/accessories/shop/eyewear/'
-# get_image(url, headers)
-
-
-import requests
 import json
 
 headers = {
